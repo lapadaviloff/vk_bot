5d369d9ba1b24628fa95db070dfe86f5d5ffe04b
lapadaviloff <48403158+lapadaviloff@users.noreply.github.com>
2021-03-08 09:33:42 +0300

    Update main.java

--- src/main/java/VK_bot/main.java
+++ src/main/java/VK_bot/main.java
@@ -6,7 +6,7 @@ import com.petersamokhin.bots.sdk.clients.Group;
 import com.petersamokhin.bots.sdk.objects.Message;
 
 public class main {
-    private static final String VK_KEY = "d94c7e5e9842ab5cd3c25b5939e7b7ef2a61e438a7122b7888ec44cfe793462050ac785acc28865069d9b";
+    private static final String VK_KEY = "здесь ваш vk_key";
 
     public static void main(String[] args) throws SteamApiException {
         Group group = new Group(195024272, VK_KEY);
